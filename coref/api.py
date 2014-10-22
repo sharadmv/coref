1b55edc47400df04b045dbba52de6f45faf0ca54
Sharad Vikram <sharad.vikram@gmail.com>
2014-10-21 23:49:58 -0700

    various fixes

--- coref/api.py
+++ coref/api.py
@@ -12,8 +12,8 @@ def fetch():
         'id': 1,
         'text': "This character's former lover, Andrea Beaumont, became the Phantasm in one animated movie. Psychologist Chase Meridian falls in love with him and his alter ego in one movie named after this character. Another movie features a crime boss named Carl Grissom and sees another villain fall from a cathedral after attempting to kidnap Vicki Vale; that man is played by Jack Nicholson. A TV series saw Burgess Meredith play the Penguin against Adam West, who played this protagonist. For 10 points, name this comic book character whose most recent movie featured Heath Ledger as The Joker and was called The Dark Knight.",
         'phrases': [
-            [0, 16],
-            [25, 29]
+            [0, 14],
+            [31, 46]
         ]
     })
 
